0936cc024920bcedec1a94213fe5103b83e1df29
mayteles <maylon.ferreira@icloud.com>
2024-02-02 18:43:36 -0800

    comment form design implemented for mobile

--- src/components/main/MainVideo.js
+++ src/components/main/MainVideo.js
@@ -3,6 +3,7 @@ import videosData from '../../data/video-details.json';
 import eye from '../../assets/Icons/views.svg';
 import likes from '../../assets/Icons/likes.svg';
 import commentLogo from '../../assets/Icons/add_comment.svg';
+import profile from '../../assets/Images/Mohan-muruge.jpg';
 
 
 const VideoItem = ({ onVideoSelect }) => {
@@ -40,31 +41,40 @@ const VideoItem = ({ onVideoSelect }) => {
             <p className="video__item-description">{video.description}</p>
         </div>
         {/* sub-comment section */}
-        <section>
+        <section className='comment'>
             <h3>3 Comments</h3>
-            <form action='/'>
-                <h4>JOIN THE CONVERSATION</h4>
-                <textarea rows="7" cols="30" name="comment">
-                    Add a new comment
-                </textarea>
-                <button type='button'>
-                    <span><img src={commentLogo} alt='comment'/></span>
-                    <span>COMMENT</span>
-                </button>
+            <form action='/' className='comment__form'>
+                <div className='comment__form-content'>
+                    <div className='comment__profile-wrapper'>
+                        <img src={profile} alt='Profile' className='comment__profile'/>
+                    </div>
+                    <div className='comment__area'>
+                        <h4 className='comment__heading'>JOIN THE CONVERSATION</h4>
+                        <textarea rows="7" cols="10" name="comment" className='comment__textarea'>
+                            Add a new comment
+                        </textarea>
+                        <button type='button' className='comment__btn'>
+                            <span className='comment__btn-imgwrapper'>
+                                <img src={commentLogo} alt='comment'/>
+                            </span>
+                            <span className='comment__btn-text'>COMMENT</span>
+                        </button>
+                    </div>
+                </div>
             </form>
-            <hr/>
+            <hr className='hr'/>
 
             <h4>{video.comments[0].name}</h4>
             <p>{video.comments[0].comment}</p>
-            <hr/>
+            <hr className='hr'/>
 
             <h4>{video.comments[1].name}</h4>
             <p>{video.comments[1].comment}</p>
-            <hr/>
+            <hr className='hr'/>
 
             <h4>{video.comments[2].name}</h4>
             <p>{video.comments[2].comment}</p>
-            <hr/>
+            <hr className='hr'/>
         </section>
     </section>
   );
