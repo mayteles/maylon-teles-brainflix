13850bcc8008fb6df8763368e1bce6bbf9ff3245
mayteles <maylon.ferreira@icloud.com>
2024-02-02 23:11:36 -0800

    Video list mobile design implemented

--- src/components/main/VideoList.js
+++ src/components/main/VideoList.js
@@ -3,17 +3,19 @@ import videoData from '../../data/videos.json';
 
 const List = ({ onVideoSelect }) => {
   return (
-    <section className="video-list">
-        <h4>NEXT VIDEOS</h4>
-        {videoData.map((video) => (
-                <div key={video.id} onClick={() => onVideoSelect(video)}>
-                    <video width="100" height="50" poster={video.image}>Your Browser does not support the video tag</video>
-                    <section>
-                        <h3>{video.title}</h3>
-                        <a href='/'><span>{video.channel}</span></a>
-                    </section>
-                </div>
-        ))}
+    <section className="video">
+      <h4 className='video__heading'>NEXT VIDEOS</h4>
+      {videoData.map((video) => (
+        <div className='video__wrapper' key={video.id} onClick={() => onVideoSelect(video)}>
+          <div className='video__img-wrapper'>
+            <video className='video__img' poster={video.image}>Your Browser does not support the video tag</video>
+          </div>
+          <section className='video__info'>
+            <h3 className='video__info-heading'>{video.title}</h3>
+            <a className='video_info-a' href='/'><span>{video.channel}</span></a>
+          </section>
+        </div>
+      ))}
     </section>
   );
 };
