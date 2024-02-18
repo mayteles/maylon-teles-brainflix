a95a88b16db80852ad007bdba38fea3c80c36ff1
mayteles <maylon.ferreira@icloud.com>
2024-02-18 15:56:20 -0800

    upload page structure

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -2,8 +2,26 @@ import React from 'react'
 
 function Upload() {
   return (
-    <div>
-    </div>
+    <section>
+        <h1>Upload Video</h1>
+        <h2>VIDEO THUMBNAIL</h2>
+        <div>
+            <video poster=''></video>
+            <form>
+                <label>TITLE YOUR VIDEO</label>
+                <br/>
+                <input placeholder='add a title to your video'/>
+                <br/>
+                <label>ADD A VIDEO DESCRIPTION</label>
+                <br/>
+                <input placeholder='add a description to your video'/>
+                <br/>
+                <button>PUBLISH</button>
+                <br/>
+                <button>CANCEL</button>
+            </form>
+        </div>
+    </section>
   )
 }
 
