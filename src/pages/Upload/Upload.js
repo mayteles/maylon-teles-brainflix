fd70f762dc001abe164c9f3cab5cb2de890ac2bb
mayteles <maylon.ferreira@icloud.com>
2024-02-19 00:07:04 -0800

    upload mobile style implemented

---
+++ src/pages/Upload/Upload.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// image for video post
+import thumbnail from '../../assets/Images/Upload-video-preview.jpg';
+
+function Upload() {
+  return (
+    <section className='upload'>
+        <h1 className='upload__heading'>Upload Video</h1>
+        <h2 className='upload__heading2'>VIDEO THUMBNAIL</h2>
+        <div className='upload__thumbnail'>
+            <video className='upload__thumbnail-video' poster={thumbnail}></video>
+            <form className='upload__form'>
+                <label className='upload__form-subtitle'>TITLE YOUR VIDEO</label>
+                <br/>
+                <input className='upload__form-input' placeholder='add a title to your video'/>
+                <br/>
+                <label className='upload__form-subtitle'>ADD A VIDEO DESCRIPTION</label>
+                <br/>
+                <input className='upload__form-input upload__form-input--large' placeholder='add a description to your video'/>
+                <br/>
+                <button className='upload__form-btn'>PUBLISH</button>
+                <br/>
+                <button className='upload__form-cancel'>CANCEL</button>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default Upload
